abd4f7402463b894e15b200147463f45b7c858be
Sahad2701 <skachhawala33@gmail.com>
2026-02-08 19:08:52 +0530

    style: Final formatting pass for platform stub

--- lib/src/platform/platform_stub.dart
+++ lib/src/platform/platform_stub.dart
@@ -2,5 +2,5 @@ import '../../trusted_time_platform_interface.dart';
 
 /// Stub function to satisfy the conditional import requirements.
 TrustedTimePlatform getPlatformInstance() => throw UnimplementedError(
-      'Cannot create a platform instance without specialized implementation.',
-    );
+  'Cannot create a platform instance without specialized implementation.',
+);
