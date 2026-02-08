7339312d838444ac1e5a02b14a8ed0620cd50f8b
Sahad2701 <skachhawala33@gmail.com>
2026-02-08 18:55:53 +0530

    feat: Add full platform support (Web/Desktop) and SPM compliance

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "trusted_time",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "trusted_time", targets: ["trusted_time"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "trusted_time",
+            dependencies: [],
+            path: "ios/Classes"
+        )
+    ]
+)
